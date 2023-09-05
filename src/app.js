f5dcb60b71b34c8f5d39460c8b9929b22e2aec79
Wilker Mizael <wilkermizael@Wilker-Macbook-Pro.local>
2023-09-05 15:03:48 -0300

    projeto inciado

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+import router from "./routes/index.js";
+
+//CONFIGURAÇÕES
+app.use(cors())
+app.use(express.json())
+//ROTAS
+app.use(router)
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=> console.log(`Servidor está rodando na porta ${port}`))
---
+++ src/database/database.js
@@ -0,0 +1,24 @@
+import pg from 'pg'
+import dotenv from 'dotenv'
+
+dotenv.config();
+const {Pool} = pg
+
+const configDataBase = {
+    connectionString: process.env.DATABASE_URL,
+}
+
+if(process.env.NODE_ENV === 'production') configDataBase.ssl = true;
+
+const db = new Pool(configDataBase);
+
+db.connect((error, client, done) => {
+  if (error) {
+    console.error("Error connecting to PostgresSQL", error);
+  } else {
+    console.log("Connected to PostgresSQL");
+    done();
+  }
+});
+
+export default db;
